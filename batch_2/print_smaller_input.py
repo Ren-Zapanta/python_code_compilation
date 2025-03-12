b3f43a367a2a8d6512cc7f69c319744d0f26cad2
Ren <renzapnta4@gmail.com>
2025-03-13 02:15:09 +0800

    Asks the user for numeric inputs

--- batch_2/print_smaller_input.py
+++ batch_2/print_smaller_input.py
@@ -1,1 +1,5 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+#Asks the user for 2 inputs
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter a number: "))
+
