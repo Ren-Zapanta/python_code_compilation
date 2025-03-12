3af0b77215240d10ddeb4e863bcf80e576610857
Ren <renzapnta4@gmail.com>
2025-03-12 22:45:02 +0800

    Gets the summation of all user inputs and prints it.

--- batch_1/sum_of_inputs.py
+++ batch_1/sum_of_inputs.py
@@ -1,8 +1,11 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
-sum = 0
+sum_of_inputs = 0
 
 for i in range(10):
-    num = float(input(f"Please input a number {i + 1}: "))
+    user_input = float(input(f"Please input a number {i + 1}: "))
+    sum_of_inputs += user_input
 
-    
+print(sum_of_inputs)
+
+
 
