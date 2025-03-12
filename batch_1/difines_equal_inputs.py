a86d7f92ea00e31d505117bbdd451f1ff0f7928f
Ren <renzapnta4@gmail.com>
2025-03-12 21:35:50 +0800

    Asks the user for two inputs

---
+++ batch_1/difines_equal_inputs.py
@@ -0,0 +1,4 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+num1 = int(input(f"Please input a number: "))
+num2 = int(input(f"Please input another number: "))
