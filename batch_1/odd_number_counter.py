15989e3dd989b6d1be8c5691b23048332032ba4e
Ren <renzapnta4@gmail.com>
2025-03-12 23:04:01 +0800

    Determines if the remainder of the input when divided by 2 is not 0. If so, it adds to the odd counter.

--- batch_1/odd_number_counter.py
+++ batch_1/odd_number_counter.py
@@ -1,7 +1,13 @@
 #Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
-odd_numbers = []
+odd_numbers = 0
 
 for i in range(10):
-    user_input = int(input(f"Please input a value {i + 1}: "))        
+    user_input = int(input(f"Please input a value {i + 1}: "))      
+
+    if user_input % 2 != 0:
+        odd_numbers += 1 
+
+print(odd_numbers)
+        
     
