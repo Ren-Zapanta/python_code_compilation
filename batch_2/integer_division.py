b4e57d696200e0021a4f695586650dbe4dd74fa8
Ren <renzapnta4@gmail.com>
2025-03-13 02:47:44 +0800

    Request inputs from the user

---
+++ batch_2/integer_division.py
@@ -0,0 +1,4 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+#Request 2 inputs from the user
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter another number: "))
