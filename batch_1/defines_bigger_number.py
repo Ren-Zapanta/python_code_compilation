e5b8cf5277da3618c4f72e415ac8401d56afc327
Ren <renzapnta4@gmail.com>
2025-03-12 21:29:36 +0800

    Prints the bigger number of the two inputs

--- batch_1/defines_bigger_number.py
+++ batch_1/defines_bigger_number.py
@@ -1,3 +1,3 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 num1 = int(input(f"Please input a number"))
-num1 = int(input(f"Please input a number"))
+num2 = int(input(f"Please input a number"))
