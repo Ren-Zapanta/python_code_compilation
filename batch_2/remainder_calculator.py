f0b8089e9433ba5002acf583ad671c1e4b1bc5b1
Ren <renzapnta4@gmail.com>
2025-03-13 02:56:40 +0800

    Uses modulus operator to obtain remainder of the division

--- batch_2/remainder_calculator.py
+++ batch_2/remainder_calculator.py
@@ -2,6 +2,9 @@
 #Asks the user for 2 inputs
 num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter another number: "))
-
+#Use modulus operator to obtain remainder
+get_remainder = num_1 % num_2
+#Print result
+print(get_remainder)
 
 
