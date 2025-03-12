c78788ae9f42c1613bbbf881b5c2166ad425c35c
Ren <renzapnta4@gmail.com>
2025-03-12 22:48:03 +0800

    Variable name fix

--- batch_1/divides_floats.py
+++ batch_1/divides_floats.py
@@ -1,7 +1,7 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
-num1 = int(input(f"Please input a number: "))
-num2 = int(input(f"Please input another number: ")) 
+num_1 = int(input(f"Please input a number: "))
+num_2 = int(input(f"Please input another number: ")) 
 
-get_quotient = num1 / num2
+get_quotient = num_1 / num_2
 
 print(get_quotient)
