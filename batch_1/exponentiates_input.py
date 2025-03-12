404caf7ddbccf6c38164c60eab741a506149df41
Ren <renzapnta4@gmail.com>
2025-03-12 22:06:11 +0800

    exponentiates the first input by the second input, then prints the result.

--- batch_1/exponentiates_input.py
+++ batch_1/exponentiates_input.py
@@ -1,3 +1,7 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 num1 = int(input(f"Please input a number: "))
 num2 = int(input(f"Please input a number to raise the first input to: "))
+
+exponentiate = num1 ** num2
+
+print(exponentiate)
