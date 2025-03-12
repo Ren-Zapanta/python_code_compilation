ee96dbe3df3e917c918757c4e4f6e7af81dbf15c
Ren <renzapnta4@gmail.com>
2025-03-13 02:54:39 +0800

    Requests 2 inputs from the user

---
+++ batch_2/remainder_calculator.py
@@ -0,0 +1,7 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+#Asks the user for 2 inputs
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter another number: "))
+
+
+
