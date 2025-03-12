047e41bdad3a38110a627324f0603cc5e856c5d8
Ren <renzapnta4@gmail.com>
2025-03-12 22:11:38 +0800

    Requests 10 inputs from the user to perform addition with.

---
+++ batch_1/sum_of_inputs.py
@@ -0,0 +1,8 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+sum = 0
+
+for i in range(10):
+    num = float(input(f"Please input a number {i + 1}: "))
+
+    
+
