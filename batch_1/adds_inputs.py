fa1fee8ca7d189fd48861570ef5be2c7be6f760f
Ren <renzapnta4@gmail.com>
2025-03-13 00:57:39 +0800

    Psuedocode

--- batch_1/adds_inputs.py
+++ batch_1/adds_inputs.py
@@ -1,7 +1,9 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+#Asks the user for 2 inputs
 num_1 = int(input(f"Please input a number: "))
 num_2 = int(input(f"Please input another number: "))
-
+#Performs addition on both user input
 get_sum = num_1 + num_2
-
+#Prints calculated sum
 print(get_sum)
