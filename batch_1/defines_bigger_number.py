999df163ea0cd6480b752e41bfdd786ed4e1c00b
Ren <renzapnta4@gmail.com>
2025-03-13 01:00:59 +0800

    Pseudocode

--- batch_1/defines_bigger_number.py
+++ batch_1/defines_bigger_number.py
@@ -1,7 +1,8 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+#Asks the user for two numeric inputs
 num_1 = int(input(f"Please input a number: "))
 num_2 = int(input(f"Please input a number: "))
-
+#Checks which input is bigger in value. Prints the bigger one.
 if num_1 > num_2:
     print(f"{num_1} is the bigger number.")
 else:
--- batch_1/difines_equal_inputs.py
+++ batch_1/difines_equal_inputs.py
@@ -1,8 +1,10 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+#Asks the user for two numeric inputs
 num_1 = int(input(f"Please input a number: "))
 num_2 = int(input(f"Please input another number: "))
-
+#Verifies if both inputs are equal in value
 if num_1 == num_2:
     print("Equal")
+#Prints equal if values are equal. Prints not equal if otherwise
 else:
     print("Not equal")
