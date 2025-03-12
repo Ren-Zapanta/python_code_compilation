d8c714042f736ec29aa63ffa9e367019b8123646
Ren <renzapnta4@gmail.com>
2025-03-12 21:58:54 +0800

    Divides

--- batch_1/divides_floats.py
+++ batch_1/divides_floats.py
@@ -1,3 +1,7 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 num1 = int(input(f"Please input a number: "))
 num2 = int(input(f"Please input another number: ")) 
+
+get_quotient = num1 / num2
+
+print(get_quotient)
