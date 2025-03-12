b6ac221c353f5334c07f210312aa636ba08bd5ec
Ren <renzapnta4@gmail.com>
2025-03-12 21:56:21 +0800

    Requests two numbers from the user to use for division.

---
+++ batch_1/divides_floats.py
@@ -0,0 +1,3 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+num1 = int(input(f"Please input a number: "))
+num2 = int(input(f"Please input another number: ")) 
