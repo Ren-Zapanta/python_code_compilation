65706d4a1bbca6c9902a4d944d4d7cf438b196ac
Ren <renzapnta4@gmail.com>
2025-03-12 22:46:57 +0800

    Variable name fixes

--- batch_1/defines_bigger_number.py
+++ batch_1/defines_bigger_number.py
@@ -1,8 +1,8 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-num1 = int(input(f"Please input a number: "))
-num2 = int(input(f"Please input a number: "))
+num_1 = int(input(f"Please input a number: "))
+num_2 = int(input(f"Please input a number: "))
 
-if num1 > num2:
-    print(f"{num1} is the bigger number.")
+if num_1 > num_2:
+    print(f"{num_1} is the bigger number.")
 else:
-    print(f"{num2} is the bigger number.")
+    print(f"{num_2} is the bigger number.")
