b95871a1420bb6d54a43a1e3628e37b59f072c6d
Ren <renzapnta4@gmail.com>
2025-03-12 22:55:48 +0800

    Requests 10 numeric inputs from the user.

---
+++ batch_1/odd_number_counter.py
@@ -0,0 +1,7 @@
+#Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+odd_numbers = []
+
+for i in range(10):
+    user_input = int(input(f"Please input a value {i + 1}: "))        
+    
