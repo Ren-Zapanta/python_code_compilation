678c51de88463455239e8e0238a7d235b30fb73c
Ren <renzapnta4@gmail.com>
2025-03-12 21:43:59 +0800

    Performs addition on the user's two inputs, and prints the sum.

--- batch_1/adds_inputs.py
+++ batch_1/adds_inputs.py
@@ -1,3 +1,7 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 num1 = int(input(f"Please input a number: "))
 num2 = int(input(f"Please input another number: "))
+
+operation = num1 + num2
+
+print(operation)
