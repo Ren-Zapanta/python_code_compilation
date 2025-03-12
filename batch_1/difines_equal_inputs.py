3da749049468e3d15970f41b0ea098a134ad4543
Ren <renzapnta4@gmail.com>
2025-03-12 22:47:31 +0800

    Variable name fixes

--- batch_1/difines_equal_inputs.py
+++ batch_1/difines_equal_inputs.py
@@ -1,8 +1,8 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
-num1 = int(input(f"Please input a number: "))
-num2 = int(input(f"Please input another number: "))
+num_1 = int(input(f"Please input a number: "))
+num_2 = int(input(f"Please input another number: "))
 
-if num1 == num2:
+if num_1 == num_2:
     print("Equal")
 else:
     print("Not equal")
