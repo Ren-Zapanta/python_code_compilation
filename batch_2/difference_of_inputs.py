f2124a746ec421cd54b72820bfc35f4fa2dbd45b
Ren <renzapnta4@gmail.com>
2025-03-13 02:34:20 +0800

    Requests 2 numeric inputs from user.

---
+++ batch_2/difference_of_inputs.py
@@ -0,0 +1,4 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+#Asks the user for inputs
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter another number: "))
