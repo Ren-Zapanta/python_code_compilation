d67f278f63ed02a77f0ab06d378b1fc4d1c641ab
Ren <renzapnta4@gmail.com>
2025-03-13 02:16:39 +0800

    Checks which user input is the smaller value.

--- batch_2/print_smaller_input.py
+++ batch_2/print_smaller_input.py
@@ -2,4 +2,9 @@
 #Asks the user for 2 inputs
 num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter a number: "))
+#Check which input is smaller 
+if num_1 > num_2:
+    print(f"{num_2} is smaller")
+else:
+    print(f"{num_1} is smaller")
 
