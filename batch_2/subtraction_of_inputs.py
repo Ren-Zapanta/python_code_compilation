b916da053fc07ff5f987fe5e4219d601764d9db0
Ren <renzapnta4@gmail.com>
2025-03-13 03:14:44 +0800

    Uses the first number as a starting value, then subtracts the remaining numbers with it. Prints the result

--- batch_2/subtraction_of_inputs.py
+++ batch_2/subtraction_of_inputs.py
@@ -6,4 +6,11 @@ for i in range(10):
     user_input = int(input(f"Please enter a number {i + 1}: "))
     numbers_list.append(user_input) #append use_inputs to numbers_list
 
+first_number = numbers_list [0] #set first number variable to first element of the numbers list '0'
 
+result = first_number #set result to the first_number variable
+
+for i in range(1, 10): #loop through the rest of the inputs
+    result -= numbers_list[i] # Subtract each number from the result
+#print result
+print(result)
