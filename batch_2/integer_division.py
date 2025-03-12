69bf40f1edc87bf5857691b1feabdcb2f74c7bce
Ren <renzapnta4@gmail.com>
2025-03-13 02:50:29 +0800

    Performs floor division on the inputs

--- batch_2/integer_division.py
+++ batch_2/integer_division.py
@@ -2,3 +2,7 @@
 #Request 2 inputs from the user
 num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter another number: "))
+#Perform floor division on the inputs
+get_quotient = num_1 // num_2
+#Print the result without decimal
+print(get_quotient)
