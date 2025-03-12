025c69c9ade1e68f3d23e1204a706a2287c8cd0a
Ren <renzapnta4@gmail.com>
2025-03-12 21:42:11 +0800

    Requests for two inputs

---
+++ batch_1/adds_inputs.py
@@ -0,0 +1,3 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+num1 = int(input(f"Please input a number: "))
+num2 = int(input(f"Please input another number: "))
