b52d6ff10fe4cc8de8b959a6300b0988ba980c69
Ren <renzapnta4@gmail.com>
2025-03-13 02:35:55 +0800

    Performs subtraction on the two inputs then prints the result

--- batch_2/difference_of_inputs.py
+++ batch_2/difference_of_inputs.py
@@ -2,3 +2,9 @@
 #Asks the user for inputs
 num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter another number: "))
+#Subtract num_1 with num_2
+get_difference = num_1 - num_2
+#print_result
+print(get_difference)
+
+
