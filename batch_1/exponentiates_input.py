8e8387c34ea6648d7bf5b7c2065ba84d2fb8705d
Ren <renzapnta4@gmail.com>
2025-03-12 22:48:48 +0800

    Variable name fix

--- batch_1/exponentiates_input.py
+++ batch_1/exponentiates_input.py
@@ -1,7 +1,7 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
-num1 = int(input(f"Please input a number: "))
-num2 = int(input(f"Please input a number to raise the first input to: "))
+num_1 = int(input(f"Please input a number: "))
+num_2 = int(input(f"Please input a number to raise the first input to: "))
 
-exponentiate = num1 ** num2
+exponentiate = num_1 ** num_2
 
 print(exponentiate)
