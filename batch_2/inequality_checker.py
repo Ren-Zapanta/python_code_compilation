66e4400926c80ed807711b68079d97243acb1655
Ren <renzapnta4@gmail.com>
2025-03-13 02:29:12 +0800

    Asks the user for 2 inputs.

---
+++ batch_2/inequality_checker.py
@@ -0,0 +1,4 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+#Asks the user for 2 inputs
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter another number: "))
