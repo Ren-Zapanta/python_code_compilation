ea355781fdb0d52a8204edd4c6d75a9f18801ca2
Ren <renzapnta4@gmail.com>
2025-03-13 22:39:59 +0800

    Filters out repated inputs

--- batch_3/first_entry_only.py
+++ batch_3/first_entry_only.py
@@ -1,6 +1,12 @@
 #Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 #Initialize list for entries
 input_list = []
-
+#Requests 10 inputs from the user
 for i in range(10):
     user_input = int(input(f"Please enter a number {i + 1}: "))
+    #verifies if input is already on the list
+    if user_input not in input_list:
+        #appends input if not yet on the list
+        input_list.append(user_input)
+#Print final list
+print(input_list)
