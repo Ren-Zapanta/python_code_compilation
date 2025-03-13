c128e2bce2e2ac4a19601ce06b1b50ea3380dfe6
Ren <renzapnta4@gmail.com>
2025-03-14 00:05:15 +0800

    verifies which input is the lowest

--- batch_3/displays_lowest_value.py
+++ batch_3/displays_lowest_value.py
@@ -1,15 +1,21 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 #Initialized empty list
-input_list = []
+lowest_value = None
 
 #Loop that runs indefinitley unless invalid input
 while True:
 
     #Requests inputs from the user using loop
     user_input = input("Please enter a value: ")
-    #Checks if user input is valid
+    #stops the loop if input is invalid
     if not user_input.isdigit():
         break
 
-    #Cnverts data type
+    #Converts data type
     user_input = int(user_input)
+    #verify is first input is None. Then, verify if succeeding inputs are less than current lowest.
+    if lowest_value is None or user_input < lowest_value:
+        #update lowest number
+        lowest_value = user_input
+#print result
+print(lowest_value)
