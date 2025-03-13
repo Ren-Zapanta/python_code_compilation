f2c50321fbc4986c93ffbf2d8450e2a108ac7dac
Ren <renzapnta4@gmail.com>
2025-03-13 21:55:05 +0800

    Determines the range using the two inputs

--- batch_2/in_between_inputs.py
+++ batch_2/in_between_inputs.py
@@ -1,3 +1,9 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+#requests 2 inoputs from the user
 num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter another number: "))
+#Determines the range using the two inputs
+ceil_input = max(num_1, num_2)
+floor_input = min(num_1, num_2)
+for i in range(floor_input + 1, ceil_input):
+    print(i)
