bb1bf3d667daf67369703e47efb557b73145fae3
Ren <renzapnta4@gmail.com>
2025-03-14 01:43:01 +0800

    Verifies if an input is duplicated. if so, it prints it.

--- batch_4/display_duplicates_only.py
+++ batch_4/display_duplicates_only.py
@@ -6,5 +6,11 @@ duplicates_list = []
 #Requests 10 inputs from the user
 for i in range(10):
     user_input = int(input(f"Please enter a number {i + 1}: "))  
+    #append user input to numbers list
     numbers_list.append(user_input)
 
+for user_input in numbers_list: 
+    if numbers_list.count(user_input) > 1 and user_input not in duplicates_list: #Verifies if input is a duplicate or not
+        duplicates_list.append(user_input) #appends input to duplicates list
+
+print(duplicates_list)
