5f1f0cfe0026aa3663c770772501b07dd41c6ad6
Ren <renzapnta4@gmail.com>
2025-03-13 21:16:30 +0800

    Iterates through the numbers_list then checks if each element is divisible by zero. If so, it adds to the even_counter

--- batch_2/evens_counter.py
+++ batch_2/evens_counter.py
@@ -6,3 +6,13 @@ for i in range(10):
     user_input = int(input(f"Please enter a value {i + 1}: "))
     #Append user entries to the empty list
     numbers_list.append(user_input)
+    #Initialize counter for even numbers
+even_count = 0
+#Iterate through each value stored in numbers_list
+for user_input in numbers_list:
+    #Checks if number is even
+    if user_input % 2 == 0:
+        #adds to the even_count
+        even_count += 1
+#Prints even numbers inputed 
+print(even_count)
