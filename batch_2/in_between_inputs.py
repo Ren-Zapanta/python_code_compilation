31a2331bcce6cea1cee20ec691810cdc8381aab3
Ren <renzapnta4@gmail.com>
2025-03-13 21:46:53 +0800

    Requests 2 numeric entries from the user.

---
+++ batch_2/in_between_inputs.py
@@ -0,0 +1,3 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter another number: "))
