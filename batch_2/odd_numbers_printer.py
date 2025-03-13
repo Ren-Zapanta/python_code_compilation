e4db9cc2cb9fe9b5c467f2196c29163b1e2b489d
Ren <renzapnta4@gmail.com>
2025-03-13 21:32:42 +0800

    Prints succeeding numbers less than 100, skip 2 steps

--- batch_2/odd_numbers_printer.py
+++ batch_2/odd_numbers_printer.py
@@ -1,3 +1,7 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 #Set variable value to 1
 numbers = 1
+#Print succeeding numbers less than 100, skip 2 steps
+while numbers <= 100:
+    print(numbers)
+    numbers += 2
